04c035187ba9bf4834b7622620d5a857ea54af74
Valentin Bellini <valenbellini1999@gmail.com>
2022-02-09 11:44:26 -0300

    Finish course

--- site/029-DOM-Manipulation-js/js/script.js
+++ site/029-DOM-Manipulation-js/js/script.js
@@ -1,45 +1,28 @@
-//Arrays
-var array = new Array();
-  array[0] = "Valentin";
-  array[1] = 2;
-  array[2] = function(name){
-    console.log("Hello " + name);
-  };
-  array[3] = {course: " HTML, CSS & JS"};
+// DOM manipulation
+// console.log(document.getElementById("title"));
+// console.log(document instanceof HTMLDocument);
 
-console.log(array);
-console.log(array[1]);
-array[2](array[0]);
-console.log(array[3].course);
+function sayHello () {
+  var name =
+   document.getElementById("name").value;
+   var message = "<h2>Hello " + name + "!</h2>";
 
-// Short hand array creation
-var names = ["Valentin", "Agustin", "Joe"];
-console.log(names);
+  // document
+  //   .getElementById("content")
+  //   .textContent = message;
 
-var namess = [
-{ name: "Valentin"},
-{ name: "Agustin"},
-Joe
-]
-console.log(namess);
+  document
+    .getElementById("content")
+    .innerHTML = message;
 
-
-for (var i=0; i < names.length; i++){
-  console.log("Hello " names[i]);
-}
-
-// Another form
-
-var names2 = {"Valentin", "Agustin", "Juan"};
-for(var name in names2){
-  console.log("Hello " + names2[name]);
-}
-
-var myObj = {
-  name: "Valentin"
-  course: "HTML/CSS/JS",
-  plataform: "Coursera"
-};
-for(var prop in myObj){
-  console.log(prop + ": " + myObj[prop]);
+  if (name === "student") {
+    var title = 
+      document
+        .querySelector("#title")
+        .textContent;
+    title += "Student!!";
+    document
+        .querySelector("h1") // Si hay muchos H1 toma el primero
+        .textContent = title;
+  }
 }
